8f6d6db8677a293d78f31cdd38628900734c506a
Jack <Jack@Corrados-MacBook-Pro.local>
2018-11-29 16:45:06 -0800

    updates

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 
-var targetNumber = Math.floor(Math.random()*101+1);
+var targetNumber = Math.floor(Math.random()*101) +1;
 var win = 0;
 var loss = 0;
 var button1 = Math.floor(Math.random() *11) +1;
@@ -12,44 +12,51 @@ var userScore = 0;
 function myFunction() {
     targetNumber =+ Math.floor(Math.random() *101) +1;
     $("#targetNumber").html(targetNumber);
-};
+
+}   
+
+function finalCount() {
+
+    if (userScore === targetNumber){
+        $("#win").html(win);
+        wins++;
+        alert("You Won!");
+    }
+    
+    else {
+        $("#loss").html(loss);
+        loss++;
+        alert("You Lose!");
+    }
+}
 
 
 $("#button-1").on("click", function() {
     userScore += button1;
     $("#crystalValue").html(button1).hide();
     $("#userScore").html("Total Score: " + userScore);
+    finalCount();
 });
 
 $("#button-2").on("click", function() {
     userScore += button2;
     $("#crystalValue").html(button2).hide();
     $("#totScore").html("#crystalValue" + button2);
-    $("#userScore").text("Total Score: " + userScore);
+    $("#userScore").html("Total Score: " + userScore);
+    finalCount();
 });
 
 $("#button-3").on("click", function() {
     userScore += button3;
     $("#crystalValue").html(button3).hide();
-    $("#userScore").text("Total Score: " + userScore);
+    $("#userScore").html("Total Score: " + userScore);
+    finalCount();
 });
 
 $("#button-4").on("click", function() {
     userScore += button4;
     $("#crystalValue").html(button4).hide();
-    $("#userScore").text("Total Score: " + userScore);
+    $("#userScore").html("Total Score: " + userScore);
+    finalCount();
 });
-
-
-
-
-
-
-// if (result === randomNumber){
-//     $("wins-lose").html("#win-lose");
-//     wins++;
-//     $("#wins").html(wins)
-//   }
-
-  
 
